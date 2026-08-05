26cb34026534143d3e5fb9e43121f0b246d63f43
Sudiksha Chugh <sudiksha2721@gmail.com>
2026-08-06 03:38:10 +0530

    chore: point at deployed backend url

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://tourist-safety-backend-tixp.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
